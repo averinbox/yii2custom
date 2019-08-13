9acfd616972fc17c3820a328a0e72782f76518ea
Evgeni <evgeni_dev@mail.ru>
2019-08-13 12:10:29 +0300

    public config

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,7 @@
  */
 
 Yii::setAlias('common', dirname(__DIR__ ) . '/src');
+Yii::setAlias('public', dirname(__DIR__ ) . '/public');
 Yii::setAlias('applications', dirname(__DIR__ ) . '/src/applications');
 Yii::setAlias('frontend', dirname(__DIR__ ) . '/src/applications/frontend');
 Yii::setAlias('backend', dirname(__DIR__ ) . '/src/applications/backend');
