9a514caef35b4cde0c042f869970bf7da3660d2c
Evgeni <evgeni_dev@mail.ru>
2019-08-13 12:05:57 +0300

    Более гибкий конфиг

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,7 +3,7 @@
  * Main bootstrap file.
  */
 
-Yii::setAlias('common', dirname(__DIR__ ). '/src');
-Yii::setAlias('applications', dirname(__DIR__ ). '/src/applications');
-Yii::setAlias('frontend', dirname(__DIR__ ). '/src/applications/frontend');
-Yii::setAlias('backend', dirname(__DIR__ ). '/src/applications/backend');
+Yii::setAlias('common', dirname(__DIR__ ) . '/src');
+Yii::setAlias('applications', dirname(__DIR__ ) . '/src/applications');
+Yii::setAlias('frontend', dirname(__DIR__ ) . '/src/applications/frontend');
+Yii::setAlias('backend', dirname(__DIR__ ) . '/src/applications/backend');
