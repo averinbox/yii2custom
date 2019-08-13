607c11415a57cfe73e83e5ffae3bb39b14055ba9
Evgeni <evgeni_dev@mail.ru>
2019-08-13 17:37:00 +0300

    Настройка bower, gii

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,3 +8,7 @@ Yii::setAlias('public', dirname(__DIR__ ) . '/public');
 Yii::setAlias('applications', dirname(__DIR__ ) . '/src/applications');
 Yii::setAlias('frontend', dirname(__DIR__ ) . '/src/applications/frontend');
 Yii::setAlias('backend', dirname(__DIR__ ) . '/src/applications/backend');
+Yii::setAlias('vendor', dirname(__DIR__ ) . '/vendor');
+Yii::setAlias('bower', dirname(__DIR__ ) . '/vendor/bower-asset');
+Yii::setAlias('bower/jquery/dist', dirname(__DIR__ ) . '/vendor/bower-asset/jquery/dist');
+Yii::setAlias('yii/gii', dirname(__DIR__ ) . '/vendor/yiisoft/yii2-gii/src');
