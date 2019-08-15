685daf930f5ea21b199597f559dcec532f6e43e6
Evgeni <evgeni_dev@mail.ru>
2019-08-15 10:47:36 +0300

    Улучшил конфиг

--- public/index.php
+++ public/index.php
@@ -3,15 +3,16 @@
 use common\components\Application;
 use Dotenv\Dotenv;
 
-require __DIR__ . '/../config/defines.php';
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../framework/Yii.php';
 
 try {
     (new Dotenv(dirname(__DIR__)))->load();
 } catch(Exception $e){
-    echo Yii::t('app', 'Создайте и сконфигурируйте файл .env на основе .env.example'); exit;
+    echo 'Создайте и сконфигурируйте файл .env на основе .env.example'; exit;
 }
+
+require __DIR__ . '/../config/defines.php';
+require __DIR__ . '/../framework/Yii.php';
 
 require __DIR__ . '/../config/bootstrap.php';
 $config = require __DIR__ . '/../config/config.php';
